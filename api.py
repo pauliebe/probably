774cefb835e164491318fb95d732826c10978981
Paula Friedrich <pauliebe@users.noreply.github.com>
2018-02-20 21:57:45 -0600

    Add loop through preposition list for search function

--- api.py
+++ api.py
@@ -1,39 +1,43 @@
 import requests
 
-def makePrepositionList(query):
+def makePrepositionList(user_input): #adds various prepositions to the query
 	prepositions = ['in', 'at', 'around']
 	preposition_list = []
 	for preposition in prepositions:
-		modified_query = '{preposition} {query}'.format(preposition=preposition, query=query)
+		modified_query = '{preposition} {query}'.format(preposition=preposition, query=user_input)
 		preposition_list.append(modified_query)
 
 	return preposition_list
 
-def searchLOC(query, preposition):
-	probably_query = preposition + query
-	loc_request = requests.get('https://www.loc.gov/pictures/search/?q=' + probably_query + '&fo=json')
-	loc_request.raise_for_status() #check response status
-	query_response_json = loc_request.json() #get JSON
+def searchLOC(search_list):
 
-	while True:
-		#loop through results to find titles with query word in it
-		for response in query_response_json['results']:
-			if probably_query in response['title']:
-				print(response['title'])
+	for item in search_list: #loop through preposition list to cover variety of phrasing
+		probably_query= 'probably {item}'.format(item=item) #add probably
+		loc_request = requests.get('https://www.loc.gov/pictures/search/?q=' + probably_query + '&fo=json')
+		loc_request.raise_for_status() #check response status
+		query_response_json = loc_request.json() #get JSON
 
-		next_page = query_response_json['pages']['next']
+		while True:
+			#loop through results to find titles with query word in it
+			for response in query_response_json['results']:
+				if probably_query in response['title']:
+					print(response['title'])
 
-		if next_page is not None: #go on to next page of responses
-			query_response_json = requests.get('https:' + next_page + '&fo=json').json()
+			next_page = query_response_json['pages']['next']
 
-		else:
-			break
+			if next_page is not None: #go on to next page of responses
+				query_response_json = requests.get('https:' + next_page + '&fo=json').json()
 
+			else:
+				break
 
+## program starts
 print ('Where do you want to search?')
 user_query = raw_input()
+query_list = makePrepositionList(user_query)
 
-print makePrepositionList(user_query)
+searchLOC(query_list)
+
 
 
 '''
