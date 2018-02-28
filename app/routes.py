2c7a4471ae731e1e1f7dd19815b90ecec93be737
Paula Friedrich <pauliebe@users.noreply.github.com>
2018-02-28 09:35:30 -0600

    Added templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def hello():
-	return "Hello_world!"
+
+def index():
+	user ={'username': 'fake user'}
+	return render_template('index.html', title='Home', user=user)
