78e135f11736883088d2ebeba1b9f44c647dad29
Paula Friedrich <pauliebe@users.noreply.github.com>
2018-04-16 15:12:06 -0500

    app shows dictionary probablies based on second word

--- app/clean.py
+++ app/clean.py
@@ -15,81 +15,104 @@ import string
 
 #define stopwords
 def define_stopwords(query):
-	punctuation = list(string.punctuation)
-	digits = list(string.digits)
-	extra = [query, '\'s', '\'\'', '``', '--', '...', 'n\'t', '\'d', '\'ll', '\'re', '\'m']
-	stop = stopwords.words('english') + punctuation + digits + extra
+    punctuation = list(string.punctuation)
+    digits = list(string.digits)
+    extra = [query, '\'s', '\'\'', '``', '--', '...', 'n\'t', '\'d', '\'ll', '\'re', '\'m']
+    prepositions = ['in', 'at', 'on']
+    stop = stopwords.words('english') + punctuation + digits + extra
+    new_stopwords = [ w for w in stop if w not in prepositions]
 
-	return stop
+    return new_stopwords
 
 def load_json(filename):
-	with open(filename) as json_data:
-		#load json file
-		parsed_json = json.load(json_data)
+    with open(filename) as json_data:
+        #load json file
+        parsed_json = json.load(json_data)
 
-		return parsed_json
+        return parsed_json
 
 #WORD COUNT FUNCTIONS
 def count_total(filename):
-	return len(load_json(filename))
+    return len(load_json(filename))
 
 def count_common(query, filename, count):
-	
-	stop = define_stopwords(query)
+    
+    stop = define_stopwords(query)
 
-	parsed_json = load_json(filename)
+    parsed_json = load_json(filename)
 
-	count_all = Counter()
+    count_all = Counter()
 
-	for item in parsed_json:
-		title = word_tokenize(item['title'].lower())
-		#create list with all terms minus stop
-		terms_stop = [term for term in title if term not in stop]
-		count_all.update(terms_stop)
+    for item in parsed_json:
+        title = word_tokenize(item['title'].lower())
+        #create list with all terms minus stop
+        terms_stop = [term for term in title if term not in stop]
+        count_all.update(terms_stop)
 
-		count_list = count_all.most_common(count)
+        count_list = count_all.most_common(count)
 
-	return count_list
+    return count_list
 
-def count_bigrams(query, filename):
+def find_nextwords(parsed_json, query):
+    stop = define_stopwords(query)
 
-	parsed_json = load_json(filename)
+    next_words = []
+    missing_query_list = []
+    for item in parsed_json:
+        title = word_tokenize(item['title'].lower().strip(']').strip('['))
+        try:
+            query_index = title.index(query)
+        except ValueError:
+            missing_query_list.append(item['title'])
+            continue
+        try:
+            next_word = title[query_index+1]
+        except IndexError:
+            next_word = 'last_word'
+            continue
+        if next_word not in stop:
+            next_words.append(next_word)
+    return next_words
 
-	count_all = Counter()
-	
-	for item in parsed_json:
-		title = word_tokenize(item['title'].lower())
-		terms_stop = [term for term in title if term]
-		
-		bigrams_list = list(bigrams(terms_stop))
-		count_all.update(bigrams_list)
-		bigrams_count_list = count_all.most_common(5)
+#next_words, missing_query_list = find_nextwords(parsed_json,stop)
 
-	return bigrams_list
+def find_phrases(parsed_json, query):
+    next_phrases = []
+    missing_query_list = []
+    last_phrases = []
+    
+    for item in parsed_json:
+        title = word_tokenize(item['title'].lower().strip(']').strip('[').strip('"'))
+        try:
+            query_index = title.index(query)
+        except ValueError:
+            missing_query_list.append(item['title'])
+            continue
+            
+        if query_index == len(title)-1:
+            last_phrase = title[:query_index]
+            last_phrases.append(title)
+        else:
+            next_phrase = title[query_index:]
+            next_phrases.append(next_phrase)
+    
+    return next_phrases, missing_query_list, last_phrases
 
-def after_query(query, filename):
-	parsed_json = load_json(filename)
-	stop = define_stopwords(query)
-	count_all = Counter()
-	next_words=[]
-	
-	for item in parsed_json:
-		title = word_tokenize(item['title'].lower())
-		try:
-			query_index = title.index(query)
-		except ValueError:
-			#print(title)
-			continue
-		try:
-			next_word = title[query_index+1]
-		except IndexError:
-			next_word = 'last_word'
-			continue
-		if next_word not in stop:
-			next_words.append(next_word)
-		count_all.update(next_words)
+def make_dict(filename, query):
+    parsed_json = load_json(filename)
+    next_words = find_nextwords(parsed_json, query)
+    next_phrases, missing_query_list, last_phrases = find_phrases(parsed_json, query)
 
-	after_query_list = count_all.most_common(15)
-	print(after_query_list)
+    categories = {}
 
-	return after_query_list
+    for item in next_words:
+        categories[item] = []
+
+    for item in next_phrases:
+        test = item[1]
+        if test in categories:
+            title_list = categories[test] 
+            #untokenize
+            title_list.append("".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in item]))
+    
+    return categories 
--- app/routes.py
+++ app/routes.py
@@ -9,18 +9,15 @@ prepositions = ['maybe', 'probably']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	title = 'test'
-	
-	return render_template('index.html', title=title, prepositions = prepositions)
+    title = 'test'
+    
+    return render_template('index.html', title=title, prepositions = prepositions)
 
 @app.route('/<preposition>')
 def detail(preposition):
-	template='submit.html'
-	filename = os.path.join(app.root_path, 'json_files/combined/', '%s.json' %(preposition))
-	count = 15
-	total = count_total(filename)
-	count_list = count_common(preposition, filename, count)
-	bigrams_list = count_bigrams(preposition, filename)
-	after_query_list = after_query(preposition, filename)
-	
-	return render_template(template, preposition=preposition, count_list=count_list, count=count, count_total = total)
+    query = preposition
+    template='detail.html'
+    filename = os.path.join(app.root_path, 'json_files/combined/', '%s.json' %(query))
+    categories = make_dict(filename, query)
+    
+    return render_template(template, preposition=preposition, categories= categories)
