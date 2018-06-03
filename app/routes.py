cf6086bb15cb4c6745f99025658193f43c71fda3
Paula Friedrich <pauliebe@users.noreply.github.com>
2018-06-03 14:20:07 -0500

    Write details info to json for easier time working with css

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
 from flask import request, make_response, render_template
 from app import app
 import os
-from app.clean import make_dict, load_json
+import json
 from lesscss import LessCSS
 
 LessCSS(media_dir='static', exclude_dirs=['img', 'src', 'json_files'], based=True, compression='x')
 prepositions = ['maybe', 'probably']
+
+def load_json(filename):
+    with open(filename) as json_data:
+        #load json file
+        parsed_json = json.load(json_data)
+
+        return parsed_json
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,9 +23,8 @@ def index():
 @app.route('/<preposition>')
 def detail(preposition):
     template='detail.html'
-    filename = os.path.join(app.root_path, 'static/json_files/', '%s.json' %(preposition)) 
-    data = load_json(filename)
-    details=make_dict(data,preposition, app.root_path)
+    filename = os.path.join(app.root_path, 'static/json_files/', '%s-details.json' %(preposition)) 
+    details = load_json(filename)
     count = len(details)
     	
     return render_template(template, preposition=preposition, details=details, count=count)
---
+++ cleaning/write_data.py
@@ -0,0 +1,19 @@
+from clean import make_dict, load_json
+import json
+import os
+
+temp_dict = {}
+
+def write_json(output_path, data):
+    with open(output_path, 'w') as file:
+     file.write(json.dumps(data))
+
+def write_data(query):
+    base_path= os.path.abspath('./app/')
+    input_path = os.path.abspath('./app/static/json_files/%s.json' %(query)) 
+    data = load_json(input_path)
+    details= make_dict(data,query, base_path)
+    output_path = os.path.abspath('./app/static/json_files/%s-details.json' %(query))
+    write_json(output_path, details)
+
+write_data('probably')
