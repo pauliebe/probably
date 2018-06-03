8733e10bda21815d992218328cf3a4196135ca13
Paula Friedrich <pauliebe@users.noreply.github.com>
2018-06-03 15:10:48 -0500

    Get frozen flask up and running

---
+++ freeze.py
@@ -0,0 +1,16 @@
+from flask_frozen import Freezer
+from app import app
+from app.routes import load_json
+import os
+
+freezer = Freezer(app)
+prepositions = ['probably', 'maybe']
+
+
+@freezer.register_generator
+def detail():
+    for preposition in prepositions:
+        yield {'preposition' : preposition}
+
+if __name__ == '__main__':
+    freezer.freeze()
